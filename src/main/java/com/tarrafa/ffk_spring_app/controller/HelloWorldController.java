dd539055b7d7a19fdc43dea13fb36cf776804db3
jeperico <joaoeperico@gmail.com>
2025-11-14 17:06:19 -0300

    feat: start services && controlers

---
+++ src/main/java/com/tarrafa/ffk_spring_app/controller/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.tarrafa.ffk_spring_app.controller;
+
+import com.tarrafa.ffk_spring_app.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+
+    @Autowired
+    private HelloWorldService helloWorldService;
+
+//    public HelloWorldController(HelloWorldService helloWorldService) {
+//        this.helloWorldService = helloWorldService;
+//    }
+
+    @GetMapping
+    public String helloWorld() {
+        return helloWorldService.helloWorld("Tarrafa");
+    }
+}
