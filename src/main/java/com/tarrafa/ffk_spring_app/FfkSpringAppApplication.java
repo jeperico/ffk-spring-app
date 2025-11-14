b11f63938f4f8e30931614c14a08c7bbb73bf500
jeperico <joaoeperico@gmail.com>
2025-11-14 13:22:44 -0300

    chore: init

---
+++ src/main/java/com/tarrafa/ffk_spring_app/FfkSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.tarrafa.ffk_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfkSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfkSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarrafa/ffk_spring_app/FfkSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarrafa.ffk_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfkSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
